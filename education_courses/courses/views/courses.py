0a56273d5424295d4b4786294454075d6445b099
Vladislav_Kashurkin <mymailkash@gmail.com>
2023-05-23 11:29:02 +0300

    0005

--- education_courses/courses/views/courses.py
+++ education_courses/courses/views/courses.py
@@ -14,6 +14,8 @@ def courses_all(request):
             search_query = request.POST.get('search')
     else:
         selected_specials = []
+        selected_devices = []
+        selected_directions = []
         search_query = None
 
     courses = Course.objects.all()
